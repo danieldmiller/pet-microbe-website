5d326efacd3456f575d39d057830e7bcfbd4b2c9
Daniel Miller <ddmiller3@wisc.edu>
2020-11-28 23:50:38 -0600

    Commit 2

---
+++ scripts.js
@@ -0,0 +1,35 @@
+function loadGif1() {
+    document.getElementById("gif1").src="images/movie1.gif";
+}
+
+function loadGif2() {
+    document.getElementById("gif2").src="images/movie2.gif";
+}
+
+function loadGif3() {
+    document.getElementById("gif3").src="images/movie3.gif";
+}
+
+function loadGif4() {
+    document.getElementById("gif4").src="images/movie4.gif";
+}
+
+function loadGif5() {
+    document.getElementById("gif5").src="images/movie5.gif";
+}
+
+function loadGif6() {
+    document.getElementById("gif6").src="images/movie6.gif";
+}
+
+function loadGif7() {
+    document.getElementById("gif7").src="images/movie7.gif";
+}
+
+function loadGif8() {
+    document.getElementById("gif8").src="images/movie8.gif";
+}
+
+function loadGif9() {
+    document.getElementById("gif9").src="images/movie9.gif";
+}
